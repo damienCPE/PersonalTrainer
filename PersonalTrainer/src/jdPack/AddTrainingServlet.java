bacbb6628d4ab6235f4aafdcbce6200df1ab9ebe
DamienCPE <dami184@live.fr>
2015-01-08 13:53:43 +0100

    backend addTraining completed

--- PersonalTrainer/src/jdPack/AddTrainingServlet.java
+++ PersonalTrainer/src/jdPack/AddTrainingServlet.java
@@ -9,37 +9,71 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.labs.repackaged.org.json.JSONArray;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 @SuppressWarnings("serial")
 public class AddTrainingServlet extends HttpServlet {
+
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		String key = req.getParameter("key");
-		System.out.println(key);
-		
 		DatastoreService datastore = DatastoreServiceFactory
 				.getDatastoreService();
-		
-		String title = "1";//req.getParameter("title");
-		String description = "jb";//req.getParameter("description");
-		String time = "1:12:00";//req.getParameter("time");
-		String idExercices = "46846844";//req.getParameter("idExercices");
-		
-		Entity Exercices = new Entity("Exercices");
-		Exercices.setProperty("title", "exercice 1");
-		Exercices.setProperty("description", "voici un exercice");
-		Exercices.setProperty("time", "1:20:00");
-		datastore.put(Exercices);
-		long domaineId = (Exercices.getKey().getId());
-		
-		idExercices = Long.toString(domaineId);
-		Entity Training = new Entity("Training");
-		Training.setProperty("title", title);
-		Training.setProperty("description", description);
-		Training.setProperty("time", time);
-		Training.setProperty("exercicesId", idExercices);
-		datastore.put(Training);
-		
-		
+		String jsonString = req.getParameter("training");
+		System.out.println(jsonString);
+		if (jsonString != null) {
+			System.out.println("Parsing json");
+			try {
+				JSONObject json = new JSONObject(req.getParameter("training"));
+				//
+				json = json.getJSONObject("training");
+				System.out.println(json);
+				String title = json.getString("title");
+				String description = json.getString("description");
+				long domaineId = json.getLong("domaineId");
+
+				if (title == null || description == null)
+					return;
+
+				Entity Training = new Entity("Training");
+				Training.setProperty("title", title);
+				Training.setProperty("description", description);
+				Training.setProperty("domaineId", domaineId);
+				datastore.put(Training);
+
+				long trainingId = (Training.getKey().getId());
+
+				JSONArray exercices = json.getJSONArray("exercices");
+				if (exercices != null) {
+					for (int i = 0; i < exercices.length(); i++) {
+						JSONObject exercice = exercices.getJSONObject(i);
+						String exerciceTitle = exercice.getString("title");
+						String exerciceDescription = exercice
+								.getString("description");
+						int time = exercice.getInt("time");
+						int repetitions = exercice.getInt("repetitions");
+
+						if (exerciceTitle == null
+								|| exerciceDescription == null)
+							return;
+
+						Entity Exercice = new Entity("Exercice");
+						Exercice.setProperty("title", exerciceTitle);
+						Exercice.setProperty("description", exerciceDescription);
+						Exercice.setProperty("time", time);
+						Exercice.setProperty("repetitions", repetitions);
+						Exercice.setProperty("trainingId", trainingId);
+
+						datastore.put(Exercice);
+					}
+				}
+				return;
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return;
 	}
 }
--- PersonalTrainer/src/jdPack/TrainingQueueServlet.java
+++ PersonalTrainer/src/jdPack/TrainingQueueServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.appengine.labs.repackaged.org.json.JSONException;
 import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 @SuppressWarnings("serial")
@@ -22,13 +23,63 @@ public class TrainingQueueServlet extends HttpServlet {
 		Map<String, Object> data = new HashMap<String, Object>();
 	    data.put( "title", "training 1" );
 	    data.put( "description", "this is a description" );
-	    data.put( "time", "1:12:00" );
-	    data.put( "idExercices", "4646446" );
-	    JSONObject json = new JSONObject(data);
+	    data.put( "domaineId", "4646446" );
+	    //JSONObject json = new JSONObject(data);
+	    
+	    /*
+	    JSONObject json = null;
+		try {
+			json = new JSONObject(""+
+			"{"+
+				 "\"training\": {"+
+				   "\"title\": \"Le titre\","+
+				   "\"description\": \"La description\","+
+				   "\"domaineId\": \"13556777\","+
+				   "\"exercices\": ["+
+				     "{"+
+				      "\"title\": \"Titre exercice\","+
+				       "\"description\": \"Description exercice\","+
+				       "\"time\": 1,"+
+				       "\"repetitions\": 1,"+
+				       "\"trainingId\": 12265688"+
+				     "},"+
+				     "{"+
+				       "\"title\": \"Titre exercice\","+
+				       "\"description\": \"Description exercice\","+
+				       "\"time\": 1,"+
+				       "\"repetitions\": 1,"+
+				       "\"trainingId\": 12265688"+
+				     "},"+
+				     "{"+
+				       "\"title\": \"Titre exercice\","+
+				       "\"description\": \"Description exercice\","+
+				       "\"time\": 1,"+
+				       "\"repetitions\": 1,"+
+				       "\"trainingId\": 12265688"+
+				     "}"+
+				   "]"+
+				 "}"+
+				"}"+
+			");");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+	    
+	    JSONObject json;
+		try {
+			json = new JSONObject(req.getParameter("training"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+	    
 		
 		Queue queue = QueueFactory.getDefaultQueue();
 		// Ajout d’une tache simple
-		TaskOptions task=TaskOptions.Builder.withUrl("/addTraining").param("add", json.toString());
+		if(json == null)return;
+		TaskOptions task=TaskOptions.Builder.withUrl("/addTraining").param("training", json.toString());
 		 queue.add(task);
 		// Ajout d’une tache simple avec des paramètres de configuration 
 		Map<String, String> headers=new HashMap<String, String>();
@@ -37,6 +88,6 @@ public class TrainingQueueServlet extends HttpServlet {
 		//...
 		queue.deleteTask("task"); 
 		//... 
-		queue.purge(); 
+		queue.purge();
 	}
 }
