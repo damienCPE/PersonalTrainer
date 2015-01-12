ececf8247a01e3036a75bf6b093539723b69af8b
Jérémy Longin <jeremy.longin01@gmail.com>
2015-01-12 19:59:34 +0100

    Update login form title text and button size

--- PersonalTrainer/war/js/connexion.js
+++ PersonalTrainer/war/js/connexion.js
@@ -10,7 +10,7 @@ function displayConnexionContent(response) {
 		Connexion.innerHTML = "<div class=\" col-md-2 col-sm-2 col-xs-2\">"
 			+ "<form class=\"navbar-form navbar-right\" style=\"display:inline-flex\""
 			+ "role=\"connexion\">"
-			+ "<p class=\"text-success\" style=\"margin-right: 10px; margin-top:6px;\">" + response.loggedUser + "</p>"
+			+ "<p class=\"text-info\" style=\"margin-right: 10px; margin-top:6px;\">" + response.loggedUser + "</p>"
 			+ "<a class=\"btn btn-default glyphicon glyphicon-log-out\" "
 			+ "href=\"http://localhost:8888" + response.logoutUrl
 			+ "\" title=\"Se déconnecter\"></a>" + "</form>" + "</div>";
@@ -19,17 +19,17 @@ function displayConnexionContent(response) {
 		Connexion.innerHTML = "<div class=\" col-md-2 col-sm-2 col-xs-2\">"
 				+ "<form class=\"navbar-form navbar-right\" style=\"width: 100%;\""
 				+ "role=\"connexion\">"
-				+ "<a class=\"btn btn-lg btn-social-icon btn-google-plus\""
+				+ "<a class=\"btn btn-default btn-social-icon btn-google-plus\""
 				+ "href=\""+ response.loginUrlGoogle +"\""
-				+ "title=\"Sign in with Google Plus\" rel=\"nofollow\"> <i "
+				+ "title=\"Se connecter avec Google Plus\" rel=\"nofollow\"> <i "
 				+ "class=\"fa fa-google-plus\"></i>"
-				+ "</a> <a class=\"btn btn-lg btn-social-icon btn-yahoo\""
+				+ "</a> <a class=\"btn btn-default btn-social-icon btn-yahoo\""
 				+ "href=\""+ response.loginUrlYahoo +"\""
-				+ "title=\"Sign in with Yahoo!\" rel=\"nofollow\"> <i "
+				+ "title=\"Se connecter avec Yahoo!\" rel=\"nofollow\"> <i "
 				+ "class=\"fa fa-yahoo\"></i>"
-				+ "</a> <a class=\"btn btn-lg btn-social-icon btn-openid\""
+				+ "</a> <a class=\"btn btn-default btn-social-icon btn-openid\""
 				+ "href=\""+ response.loginUrlOpenId +"\""
-				+ "title=\"Sign in with OpenId\" rel=\"nofollow\"> <i "
+				+ "title=\"Se connecter avec OpenId\" rel=\"nofollow\"> <i "
 				+ "class=\"fa fa-openid\"></i>" + "</a>" + "</form>" + "</div>";
 	}
 }
